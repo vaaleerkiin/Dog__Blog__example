6691ddf6fd0b9ba8ea6d1e4172b27685aeffaa30
Valera Kamelkov <tahaburrent@gmail.com>
2023-02-10 23:34:44 +0200

    ~

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -504,8 +504,8 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"8lqZg":[function(require,module,exports) {
 var _mainGallary = require("./js/main-gallary");
-var _burgerMenu = require("./js/burger-menu");
 var _translate = require("./js/translate");
+var _burgerMenu = require("./js/burger-menu");
 var _news = require("./js/news");
 
 },{"./js/main-gallary":"lneWe","./js/burger-menu":"egJfa","./js/translate":"cybGd","./js/news":"2xmvE"}],"lneWe":[function(require,module,exports) {
@@ -2981,12 +2981,16 @@ const refs = {
     MenuBtnaboutMe: document.querySelector("[data-menu-aboutMe]"),
     MenuBtnSoc: document.querySelector("[data-menu-soc]"),
     MenuBtnContact: document.querySelector("[data-menu-contact]"),
-    menu: document.querySelector("[data-menu]")
+    menu: document.querySelector("[data-menu]"),
+    menuBtnEn: document.querySelector("#menu__en"),
+    menuBtnUa: document.querySelector("#menu__ua")
 };
 refs.openMenuBtn.addEventListener("click", toggleModal);
 refs.MenuBtnaboutMe.addEventListener("click", toggleModal);
 refs.MenuBtnSoc.addEventListener("click", toggleModal);
 refs.MenuBtnContact.addEventListener("click", toggleModal);
+refs.menuBtnEn.addEventListener("click", toggleModal);
+refs.menuBtnUa.addEventListener("click", toggleModal);
 function toggleModal() {
     document.body.classList.toggle("menu-open");
     refs.menu.classList.toggle("is-hidden-menu");
@@ -3044,23 +3048,19 @@ const refs = {
 };
 refs.navBtnEn.addEventListener("click", ()=>{
     localStorage.setItem("lang", "en");
-    location.reload();
-// ChangeLang();
+    ChangeLang();
 });
 refs.navBtnUa.addEventListener("click", ()=>{
     localStorage.setItem("lang", "ua");
-    location.reload();
-// ChangeLang();
+    ChangeLang();
 });
 refs.menuBtnEn.addEventListener("click", ()=>{
     localStorage.setItem("lang", "en");
-    location.reload();
-// ChangeLang();
+    ChangeLang();
 });
 refs.menuBtnUa.addEventListener("click", ()=>{
     localStorage.setItem("lang", "ua");
-    location.reload();
-// ChangeLang();
+    ChangeLang();
 });
 function ChangeLang() {
     let lang = localStorage.getItem("lang");
